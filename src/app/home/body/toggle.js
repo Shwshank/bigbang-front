a633802435d2880a3ee27d2d5e2fad61fbc3c9aa
MadhuSrivastava <madhu.srivastava@qcin.org>
2017-09-26 18:08:37 +0530

    login background

--- src/app/home/body/toggle.js
+++ src/app/home/body/toggle.js
@@ -20,8 +20,25 @@
         });
     });
     */
-    $(document).ready(function(){
+ /*   $(document).ready(function(){
     $('#toggle-menu').click(function(){
         $('#menu').toggle();
     });
+});
+
+*/
+
+$(document).ready(function(){
+    $("button.expansion-btn").click(function (){
+        classes = this.className;
+        var divNumber = classes.slice(-1);
+        var toGetId = "#div-"+divNumber;
+        if ($(toGetId).hasClass("expanded-div")){
+         $(".normal-div").removeClass("compressed-div expanded-div");
+        }
+        else{
+         $(".normal-div").removeClass("compressed-div expanded-div").addClass("compressed-div");;
+         $(toGetId).removeClass("compressed-div").addClass("expanded-div");    
+        }  
+      });
 });

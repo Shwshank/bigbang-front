6306a1a28b7e720494578475f9f40964f23ae7ae
MadhuSrivastava <madhu.srivastava@qcin.org>
2017-10-11 18:44:10 +0530

    Costing Components

--- src/app/home/body/toggle.js
+++ src/app/home/body/toggle.js
@@ -41,4 +41,16 @@ $(document).ready(function(){
       }, function(){
           $(this).find('.action-button__text--show').attr('class', 'action-button__text--hide');
       });
+
+
+
+        $('.collapse.in').prev('.panel-heading').addClass('active');
+        $('#accordion, #bs-collapse')
+          .on('show.bs.collapse', function(a) {
+            $(a.target).prev('.panel-heading').addClass('active');
+          })
+          .on('hide.bs.collapse', function(a) {
+            $(a.target).prev('.panel-heading').removeClass('active');
+          });
+    
 });
